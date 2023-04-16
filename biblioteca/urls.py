c0ef7b62ff1e1d511ef89b13bda34c81aa979449
RafaelGarciaFranca <128322055+RafaelGarciaFranca@users.noreply.github.com>
2023-04-16 10:52:30 -0300

    Django  - views
    Ajuste das views, urls e adaptações nos templates.

--- biblioteca/urls.py
+++ biblioteca/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
-from biblioteca.views import index, inicio
+from biblioteca.views import *
 
 urlpatterns = [
     path('', index),
-    path('inicio/', inicio, name="inicio")
+    path('inicio', inicio, name="inicio"),
+    path('leitor-geral/', leitor_geral, name='leitor-geral'),
+    path('cadastro-leitor/', cadastro_leitor, name='cadastro-leitor'),
+    path('acervo-geral/', acervo_geral, name='acervo-geral'),
+    path('cadastro-acervo/', cadastro_acervo, name='cadastro-acervo'),
+    path('emprestimo/', emprestimo, name='emprestimo'),
+    path('cadastro-emprestimo', cadastro_emprestimo, name='cadastro-emprestimo')
 ]
