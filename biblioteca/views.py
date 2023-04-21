48b5f5bce65792cb738b7c4250876d02b8283a4a
RafaelGarciaFranca <128322055+RafaelGarciaFranca@users.noreply.github.com>
2023-04-21 17:53:55 -0300

    leitor paginacao e filtros
    inclusão de novas features na página 'leitor-geral', sendo elas, inclusão de paginação e filtros.

--- biblioteca/views.py
+++ biblioteca/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator
 from biblioteca.models import TbLeitor
 
 def index(request):
@@ -9,7 +10,28 @@ def inicio(request):
 
 def leitor_geral(request):
     leitor = TbLeitor.objects.all()
-    return render(request,'biblioteca/wp31_leitor-geral.html',{"leitor": leitor})
+    #filtros
+    id = ""
+    telefone = ""
+    nome = ""
+    if "id" in request.GET:
+        id = request.GET['id']
+        if id:
+            leitor = leitor.filter(leitor_id__icontains=id)
+    if "nome" in request.GET:
+        nome = request.GET['nome']
+        if nome:
+            leitor = leitor.filter(nome__icontains=nome)
+    if "telefone" in request.GET:
+        telefone = request.GET['telefone']
+        if telefone:
+            leitor = leitor.filter(telefone__icontains=str(telefone))
+    #paginação
+    paginator = Paginator(leitor, 5)  # Show 5 contacts per page.
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+    return render(request,'biblioteca/wp31_leitor-geral.html',
+                  {"leitor": page_obj, "id":id,"nome":nome,"telefone":telefone})
 
 def cadastro_leitor(request):
     return render(request, 'biblioteca/wp32_novo-leitor.html')
